76fd2fa8184af0d5e8fcb95234b51cd11c7b769a
fufikfx <dolezaltomas828@atomicmail.io>
2026-04-28 22:31:13 +0200

    Delete crypto-terminal-app directory

--- crypto-terminal-app/Sources/crypto-terminal-app/ContentView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        TerminalWebView(telemetryPort: ProcessInfo.processInfo.environment["TELEMETRY_WS_PORT"])
-            .ignoresSafeArea()
-    }
-}
-
--- crypto-terminal-app/Tests/crypto-terminal-appTests/crypto_terminal_appTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-@testable import crypto_terminal_app
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    // Swift Testing Documentation
-    // https://developer.apple.com/documentation/testing
-}
